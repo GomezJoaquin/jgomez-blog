7ce1274d0f1af245f18cb33ea73a6353baa97e2f
Joaquín <ggomezjjoaquin@gmail.com>
2024-03-24 17:39:55 -0300

    Add:new index

--- docs/_nuxt/C8X4k0Ir.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./6mP_EsO_.js";import"./NCnxfeCb.js";import"./BsYmvPZw.js";import"./BYVTYy-F.js";export{o as default};
--- docs/_nuxt/CzEpGLzP.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./BgkZdr-h.js";import"./NCnxfeCb.js";export{m as default};

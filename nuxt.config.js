bc703603851ad0208c45ae0fad0b5154e9429ad0
Joaquín <ggomezjjoaquin@gmail.com>
2024-03-27 00:56:42 -0300

    Add generated static files for GitHub Pages deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
     base: '/jgomez-blog/' // Configura la base URL para GitHub Pages
   },
   generate: {
-    dir: 'docs' // Carpeta donde se generará la aplicación estática
+    dir: 'dist' // Carpeta donde se generará la aplicación estática
   },
   target: 'static' // Configura Nuxt.js para generar una aplicación estática
 }

3e719f8af43c0b9397e807c2b238d72357624182
Joaquin <ggomezjjoaquin@gmail.com>
2024-04-02 00:32:47 -0300

    Fix:Arreglo de enlaces relativos

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,8 +4,5 @@ export default {
   router: {
     base: '/jgomez-blog/' // Configura la base URL para GitHub Pages
   },
-  generate: {
-    dir: 'docs' // Carpeta donde se generará la aplicación estática
-  },
   target: 'static' // Configura Nuxt.js para generar una aplicación estática
 }

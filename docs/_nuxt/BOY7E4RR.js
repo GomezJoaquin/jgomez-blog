0b35c6d69c2edc5f45453849465631f47fefa228
Joaquín <ggomezjjoaquin@gmail.com>
2024-04-02 18:56:38 -0300

    Cambio de folder docs

---
+++ docs/_nuxt/BOY7E4RR.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./CWfovWDV.js";import"./gUplRhHd.js";export{m as default};
---
+++ docs/_nuxt/D8454NWj.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./HjFHFrBF.js";import"./gUplRhHd.js";import"./BsYmvPZw.js";import"./D_1Nw_fD.js";export{o as default};
